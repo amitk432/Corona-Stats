c76abd5d1b76f3961dd621cfada810ae2fc1d16d
Amit Kumar <amitkumar0432@gmail.com>
2021-05-10 23:49:03 +0530

    included splash_screen

--- app/src/main/java/com/example/corona/MainActivity.java
+++ app/src/main/java/com/example/corona/MainActivity.java
@@ -38,8 +38,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         dataList = new ArrayList<>() ;
         recyclerView = findViewById(R.id.recyclerView) ;
 
